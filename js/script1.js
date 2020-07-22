1e357aa7875062317bb8d3f59dcb938f9dca51b8
FoRA <fora22@naver.com>
2020-07-22 15:47:26 +0900

    7월 22일

---
+++ js/script1.js
@@ -0,0 +1,20 @@
+alert("1");
+// 주석
+
+// const:상수적용(변하지 않는 숫자로 만듬)
+
+// 엄격모드: const처럼 뭔가 변하지 않았으면 하는거에 사용
+let message
+message = 'hello'
+allert(message)
+allert('message')
+// let은 변수를 선언하는 함수이다. 한줄에 여러 변수 선언 가능
+myVeryLongName
+my_very_long_name
+
+let age = prompt('나이를 입력해주세요.',100);
+
+let password = prompt('비번 입력');
+if (password == value){
+
+}
